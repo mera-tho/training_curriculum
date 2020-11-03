6ead59b230f0fbec236d54a76ed095b583752e9a
mera-tho <dipo1192@yahoo.co.jp>
2020-11-03 16:03:52 +0900

    issue2

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(params_plan)
+    create.plan(params_plan)
     redirect_to action: :index
   end
 
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, :date => (@todays_date+x).day, plans: today_plans}
+      days = { month: (@todays_date + x).month, date:(@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
